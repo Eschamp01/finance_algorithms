6f3451199e6741c82d5a8500f99c52cf985ea02d
Edward Schamp <edwardjls@gmail.com>
2023-01-10 22:54:22 +0000

    improve file structure

---
+++ Stock_Algos/static/Stock_Algos/js/chart.js
@@ -0,0 +1,42 @@
+const ctx = document.getElementById('stockchart');
+
+(async function() {
+
+    new Chart(
+        ctx,
+        {
+        type: 'line',
+        options: {
+            animation: false,
+            elements: {
+            point:{
+                radius: 0
+                },
+            },
+            plugins: {
+            title: {
+                display: true,
+                text: ticker + ' Stock Chart'
+            },
+            legend: {
+                display: false
+            },
+            tooltip: {
+                //enabled: false
+            }
+            }
+        },
+        data: {
+            labels: time,
+            datasets: [
+            {
+                label: 'Closing price ($)',
+                data: y_axis,
+                borderColor: '#FF6384',
+                borderWidth: 2,
+            }
+            ]
+        }
+        }
+    );
+    })();
