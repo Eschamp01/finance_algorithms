e89e3e5f8e2444760a427e316664e725ef5b3c6c
Edward Schamp <edwardjls@gmail.com>
2023-01-12 23:43:51 +0000

    finance stuff

--- Backend_strategies/test.py
+++ Backend_strategies/test.py
@@ -1,2 +1,13 @@
-print("Edward is amazing")
-print("hi")
+import ...
+
+
+# first use list, then use API later. 
+# We want an API to get real-time data, but after a few strategies are defined
+
+price_data = [140, 141, 142, 142, 1234, 123, 123, 123, 1231]
+
+
+def moving_average(stock, start_date, end_date):
+
+    # return 20 day MA, 40 day MA and buy and sell times
+
