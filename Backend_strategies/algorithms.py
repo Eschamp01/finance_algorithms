883f2afb0a973c8e6409e821645bb3194b5f718c
Edward Schamp <edwardjls@gmail.com>
2023-01-18 19:00:12 +0000

    Backend structuring
    Added the below files:
    "data_classes.py" - to structure data model, and graph parameters
    "algorithms.py" - for all algorithm functions
    "calculate_params.py" to get graph params from an algorithm of choice

---
+++ Backend_strategies/algorithms.py
@@ -0,0 +1,21 @@
+# Define all algorithm code here. If it gets too long, could separate by strategy.
+
+def moving_average(y_vals, params):
+    """Execute moving average strategy for given params. Return all desired internally calculated metrics."""
+    low_ma = y_vals.rolling(window=params['low_ma']).mean()
+    high_ma = y_vals.rolling(window=params['high_ma']).mean()
+    # CODE HERE
+    # Whenever the lower moving average crosses the higher moving average:
+    # If you haven't bought yet (not in a position):
+    #   If lower went from low to high, buy.
+    # If you're in a position:
+    #   If lower went from high to low, sell. 
+    # Return metrics (lower_ma, higher_ma), buy dates, sell dates, and profit. Dummy variables assigned below:
+
+    buy_dates = ["2022-01-15", "2022-02-15", "2022-03-15", "2022-04-15", "2022-05-15"]
+    sell_dates = ["2022-01-25", "2022-02-25", "2022-03-25", "2022-04-25", "2022-05-25"]
+    profit = [-10.0, 5.0, -3.0, 6.0, 4.0] # same length as sell_indices, since whebever you sell you have profit/loss
+
+    metrics = {'low_ma': low_ma, 'high_ma': high_ma, 'buy_dates': buy_dates, \
+               'sell_dates':sell_dates, 'profit':profit}
+    return metrics
--- Backend_strategies/calculate_params.py
+++ Backend_strategies/calculate_params.py
@@ -1,6 +1,7 @@
 import pdb
 import yfinance as yf
 from data_classes import DataParams, ModelParams, GraphParams
+from algorithms import moving_average
 
 def get_data(data, model):
     """
@@ -16,28 +17,8 @@ def get_data(data, model):
     return x_label, x_vals, y_label, y_vals
 
 
-def moving_average(y_vals, params):
-    """Execute moving average strategy for given params. Return all desired internally calculated metrics."""
-    low_ma = y_vals.rolling(window=params['low_ma']).mean()
-    high_ma = y_vals.rolling(window=params['high_ma']).mean()
-    # CODE HERE
-    # Whenever the lower moving average crosses the higher moving average:
-    # If you haven't bought yet (not in a position):
-    #   If lower went from low to high, buy.
-    # If you're in a position:
-    #   If lower went from high to low, sell. 
-    # Return metrics (lower_ma, higher_ma), buy dates, sell dates, and profit. Dummy variables assigned below:
-
-    buy_dates = ["2022-01-15", "2022-02-15", "2022-03-15", "2022-04-15", "2022-05-15"]
-    sell_dates = ["2022-01-25", "2022-02-25", "2022-03-25", "2022-04-25", "2022-05-25"]
-    profit = [-10.0, 5.0, -3.0, 6.0, 4.0] # same length as sell_indices, since whebever you sell you have profit/loss
-
-    metrics = {'low_ma': low_ma, 'high_ma': high_ma, 'buy_dates': buy_dates, \
-               'sell_dates':sell_dates, 'profit':profit}
-    return metrics
-
-
 def get_graph_params(ticker, interval, start, end, model, params):
+    "Returns all required graph parameters. Backend algorithm execution master function."
     data = yf.download(ticker=ticker, interval=interval, start=start, end=end)
     x_label, x_vals, y_label, y_vals = get_data(data, model)
 
