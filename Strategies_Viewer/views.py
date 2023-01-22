95621c1b57f0e3e59029afd52febcc168d5c57b5
Edward Schamp <edwardjls@gmail.com>
2023-01-22 16:44:37 +0000

    view changes

--- Strategies_Viewer/views.py
+++ Strategies_Viewer/views.py
@@ -1,28 +1,15 @@
 from django.shortcuts import render
 from .models import StockData
 import pandas as pd
+import sys
+sys.path.append("../Backend_strategies")
+from Backend_strategies.calculate_params import get_graph_params
 import pdb
-
 
 def home(request):
     return render(request, 'Strategies_Viewer/home.html')
 
 def chart_view(request):
-    # data_type = 'close'
-    # time = StockData.objects.all().filter(symbol='AAPL').values_list('date', flat=True)
-    # y_axis = StockData.objects.all().filter(symbol='AAPL').values_list(data_type, flat=True)
-
-    # time_vals = []
-    # close_vals = []
-    # for stock in StockData.objects.all():
-    #     time_vals.append(stock.date)
-    #     close_vals.append(stock.close)
-
-    # return render(request, 'Strategies_Viewer/chart.html', {
-    #     'time': time_vals,
-    #     'y_axis': close_vals,
-    #     })
-
     stocks = StockData.objects.all().values()
     df = pd.DataFrame(stocks)
     time = df.date.tolist()
@@ -30,7 +17,25 @@ def chart_view(request):
     close = df.close.tolist()
     ticker = df.symbol.tolist()[0]
     output_data = {'time' : time_isoformat, 'y_axis' : close, 'ticker' : ticker}
-    return render(request, 'Strategies_Viewer/chart.html', output_data)
 
+    return render(request, 'Strategies_Viewer/chart.html', output_data) 
 
+def future_chart_view(request, data_params, model_params):
+    # Remaining front-end development pipeline:
+    # 1. get data_params and model_params from frontend
+    # 2. develop chart options for all parameters in graph_params
+    # 3. host website online, allowing link to be shared
+    # 4. market research to optimise the interface (survey of friends opinions)
+    # 5. implement features extracted from survey results
+    data_params = {'ticker':"AAPL", 'start':"2022-01-01", 'end':"2023-01-01", 'interval':"1h"}
 
+    model_params = {'name':"Moving Average", 'low_ma':20, 'high_ma':40}
+
+    graph_params = get_graph_params(ticker=data_params['ticker'], 
+                                    interval=data_params['interval'], 
+                                    start=data_params['start'], 
+                                    end=data_params['end'],
+                                    model=model_params['name'], 
+                                    params=model_params)
+
+    return render(request, 'Strategies_Veiwer/chart.html', graph_params)
